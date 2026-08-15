a98967306235ff60537d0c10915cd349b2965884
ediga hariprasad <ehariprasad2658@gmail.com>
2026-08-15 13:12:06 +0530

    creating new front-end pipeline

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello from React Frontend!</h1>
+      <p>This is a sample app deployed on AWS EKS 🚀</p>
+    </div>
+  );
+}
+
+export default App;
